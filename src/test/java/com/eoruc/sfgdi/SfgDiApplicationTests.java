e0b6306694d373dba30f92683cc93a6c2290887e
emirhan.oruc <emirhan.oruc@hotmail.com>
2023-10-07 15:30:21 +0300

    Added Maven project

---
+++ src/test/java/com/eoruc/sfgdi/SfgDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eoruc.sfgdi;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
